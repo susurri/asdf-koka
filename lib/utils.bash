e3d2ac48487c183276a31a89029e9c4f87be39c1
susurri <susurrus.silent@gmail.com>
2023-04-27 07:30:59 +0900

    first implementation

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for koka.
 GH_REPO="https://github.com/koka-lang/koka"
 TOOL_NAME="koka"
 TOOL_TEST="koka --version"
@@ -14,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if koka is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -26,45 +24,47 @@ sort_versions() {
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		grep -o -E 'refs/tags/v[0-9.]*$' | cut -d/ -f3- |
+		sed 's/^v//'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if koka has other means of determining installable versions.
 	list_github_tags
 }
 
 download_release() {
-	local version filename url
+	local version filename
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for koka
-	url="$GH_REPO/archive/v${version}.tar.gz"
-
 	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+
+	git clone --depth 1 --recursive -b "v$version" "$GH_REPO" "$filename" || fail "Could not download $GH_REPO"
+	chmod -R u+w "$filename"
 }
 
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="$3"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
 	fi
 
 	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		mkdir -p "$install_path/bin"
+		mkdir -p "$install_path/share/koka/v$version"
+		cd "$ASDF_DOWNLOAD_PATH"
+		stack update
+		stack build
+		cp -r samples support "$install_path/"
+		cp -r kklib lib "$install_path/share/koka/v$version/"
+		stack install --local-bin-path "$install_path/bin"
 
-		# TODO: Assert koka executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
